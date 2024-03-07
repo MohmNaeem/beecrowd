7b329a868c1d18ff63187c4da8acdc54fbb00a58
Mohammad Naeem <mdnaeemsarker56@gmail.com>
2024-03-07 22:36:32 +0600

    solved 1144

---
+++ 1144.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int i;
+    for (i = 1; i <= n; i++)
+    {
+
+        printf("%d %d %d\n", i, i * i, i * i * i);
+        printf("%d %d %d\n", i, i * i + 1, i * i * i + 1);
+    }
+}
